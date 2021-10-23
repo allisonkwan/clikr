fa747d5f552ef343cfbd6e14f8bd1030e9a19bbc
allisonkwan <akwan2020@gmail.com>
2021-10-23 16:57:21 -0400

    Figure out rest api

---
+++ api/models/todoModel.js
@@ -0,0 +1,25 @@
+'use strict';
+// Import mongoose
+    const mongoose = require('mongoose');
+
+// Declare schema and assign Schema class
+    const Schema = mongoose.Schema;
+
+// Create Schema Instance and add schema propertise
+    const TodoSchema = new Schema({
+        websiteUrl: {
+            type:String,
+            required:true,
+        },
+        userActionType: {
+            type:String,
+            required:true,
+        },
+        createdOn: {
+            type:Date,
+            default:Date.now,
+        }
+    });
+
+// create and export model
+module.exports = mongoose.model("todoModel", TodoSchema);
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// require express and bodyParser
+const express = require('express');
+//const bodyParser = require('body-parser');
+
+// Import DB Connection
+require("./config/db");
+
+// create express app
+const app = express();
+
+// use bodyParser middleware on express app
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+// Import API route
+var routes = require('./api/routes/todoRoutes'); //importing route
+routes(app);
+
+// define port to run express app
+const port = process.env.PORT || 3000;
+
+
+
+
+// Add endpoint
+app.get('/', (req, res) => {
+res.send("Hello World");
+});
+
+// Listen to server
+app.listen(port, () => {
+
+console.log(`Server running at http://localhost:${port}`);
+});
